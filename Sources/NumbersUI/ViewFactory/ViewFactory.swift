daa2109476592964831e3e07a7641cd4ff5d6cf2
Michel Goñi <mgoni@wabiproject.com>
2023-01-24 11:24:51 +0100

    Added implementations as spm for a view factory

--- Sources/NumbersUI/ViewFactory/ViewFactory.swift
+++ Sources/NumbersUI/ViewFactory/ViewFactory.swift
@@ -5,4 +5,20 @@
 //  Created by Michel Goñi on 23/1/23.
 //
 
-import Foundation
+
+import NumbersInjector
+import SwiftUI
+
+public struct ViewFactory {
+
+    var injector: NumbersInjector { .shared }
+    init() {}
+}
+
+@available(iOS 13.0, *)
+public extension ViewFactory {
+
+    func make<Content: View>(@ViewBuilder content: @escaping () -> Content) -> some View {
+        LazyView(build: content)
+    }
+}
